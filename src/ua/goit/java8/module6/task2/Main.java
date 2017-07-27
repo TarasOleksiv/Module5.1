a93010cfd58bc181dcb58f61b219c7676e8dbd10
Taras Oleksiv <toleksiv69@gmail.com>
2017-07-28 01:01:06 +0300

    Commit task2: create 2 methods to draw the star

--- src/ua/goit/java8/module6/task2/Main.java
+++ src/ua/goit/java8/module6/task2/Main.java
@@ -7,8 +7,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -23,31 +21,42 @@ public class Main extends Application {
     public static final double SQUARE = 90;     // прямий кут в градусах
     public static final int POLYGON = 5;        // кількість сторін многокутника
 
-    private static double x;
-    private static double y;
-    private static double radius;
+    private static double x;        // координата Х центру зірки
+    private static double y;        // координата Y центру зірки
+    private static double radius;   // радіус зірки
+    private static String method;   // метод виведення зображення
+                                    // 1 - простіший метод: 5 ліній із самоперетинами
+                                    // 2 - складніший метод: 10 ліній без самоперетинів
+
+    private static double[] vertexesX = new double[2*POLYGON+1];        // масив координат Х зовнішніх вершин зірки
+    private static double[] vertexesY = new double[2*POLYGON+1];        // масив координат Y зовнішніх вершин зірки
+    private static double[] vertexes_outX = new double[POLYGON+1];      // масив координат Х внутрішніх вершин зірки
+    private static double[] vertexes_outY = new double[POLYGON+1];      // масив координат Y внутрішніх вершин зірки
+    private static double[] vertexes_inX = new double[POLYGON];         // масив координат Х зовнішніх + внутрішніх вершин зірки
+    private static double[] vertexes_inY = new double[POLYGON];         // масив координат Y зовнішніх + внутрішніх вершин зірки
+
+    private Pane root = new Pane();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setWidth(WIDTH);
         primaryStage.setHeight(HEIGHT);
-
         primaryStage.setTitle("Зірочка");
-        Pane root = new Pane();
+
+        // отримуєм вхідні дані
         getInput();
 
-        // Перевіряєм чи зірка поміститься на сцені.
-        // Якщо так - генеруєм зірку, якщо ні - генеруєм попередження
-
-        if (checkSize(x,y,radius)) {
-            root.getChildren().addAll(generateLines(x,y-radius,radius));
-            double radius_pentagon = radius * Math.tan(Math.toRadians(SQUARE - CIRCLE/POLYGON));
-            //System.out.println(radius_pentagon);
-            //System.out.println(y+radius_pentagon);
-            root.getChildren().addAll(generatePentagon(x,y,radius_pentagon));
-        } else {
-            root.getChildren().addAll(generateWarning());
+        // обираємо метод виводу зображення зірки
+        switch (method){
+            case "1":
+                // 5 ліній із самоперетинами
+                method1();
+                break;
+            default:
+                // 10 ліній без самоперетинів
+                method2();
+                break;
         }
 
         Scene scene = new Scene(root);
@@ -59,7 +68,44 @@ public class Main extends Application {
         launch(args);
     }
 
-    // Генерація великої зірки через масив ліній
+    // 5 ліній з самоперетинами, простіший метод
+    private void method1(){
+        // Перевіряєм чи зірка поміститься на сцені.
+        // Якщо так - генеруєм зірку, якщо ні - генеруєм попередження
+        if (checkSize(x,y,radius)) {
+            root.getChildren().addAll(generateLines(x,y-radius,radius));
+        } else {
+            root.getChildren().addAll(generateWarning());
+        }
+    }
+
+    // 10 ліній без самоперетинів, складніший метод
+    private void method2(){
+        // Перевіряєм чи зірка поміститься на сцені.
+        // Якщо так - генеруєм зірку, якщо ні - генеруєм попередження
+        if (checkSize(x,y,radius)) {
+
+            // генеруєм масив зовнішніх вершин (6)[6-а співпадає з 1-ю]
+            getVertexesOut(x,y-radius,radius);
+
+            // менший радіус внутрішнього п'ятикутника (тоерема синусів):
+            // radius_pentagon = radius * sin(18°))/sin(126°)
+            double radius_pentagon = radius * Math.sin(Math.toRadians(SQUARE - CIRCLE/POLYGON))/Math.sin(Math.toRadians(2*SQUARE - CIRCLE/(2*POLYGON) - CIRCLE/(4*POLYGON)));
+
+            // генеруєм масив внутрішніх вершин (5)
+            getVertexesIn(x,y,radius_pentagon);
+
+            // об'єднуєм зовнішні + внутрішні вершини через одну в один масив
+            getVertexes();
+
+            // сполучаєм всі вершини між собою послідовно і виводим масив ліній
+            root.getChildren().addAll(getLines(vertexesX,vertexesY));
+        } else {
+            root.getChildren().addAll(generateWarning());
+        }
+    }
+
+    // Генерація великої зірки через масив ліній (всього 5 ліній) - перший метод
     private Line[] generateLines(double x, double y, double radius){
         Line[] lines = new Line[POLYGON];
         double deltaX;      // зміщення по горизонталі до наступної вершини (через одну по колу, тому множник 2):  R * cos(90° + 2 * 72°); 72° = 360° / 5
@@ -80,56 +126,62 @@ public class Main extends Application {
         return lines;
     }
 
-    // Генерація малого п'ятикутника через масив ліній
-    private Line[] generatePentagonTest(double x, double y, double radius){
-        Color color = Color.color(1,
-                0,
-                0);
-        Line[] lines = new Line[POLYGON];
+    // визначаєм координати зовнішніх вершин зірки
+    private void getVertexesOut(double x, double y, double radius){
         double deltaX;      // зміщення по горизонталі до наступної вершини :  R * cos(90° + 72°); 72° = 360° / 5
         double deltaY;      // зміщення по вертикалі до наступної вершини :  R * (1 - sin(90° + 72°)); 72° = 360° / 5
-        double x_old = x;   // стартова позиція по горизонталі під центром зірки
-        double y_old = y;   // стартова позиція по вертикалі під центром зірки
-        //System.out.println("i = -1 ; x: " + x_old + " ; y: " + y_old);
-        for (int i = 0; i < lines.length; i++){
-            deltaX = radius * Math.cos(Math.toRadians(3*SQUARE + (i+1)*CIRCLE/POLYGON));
-            deltaY = radius * (1 - Math.sin(Math.toRadians(3*SQUARE + (i+1)*CIRCLE/POLYGON)));
-            // сполучаєм дві послідовні по циклу вершини
-            lines[i] = new Line(x_old,
-                    y_old,
-                    x + deltaX,
-                    y + deltaY);
-            lines[i].setFill(Paint.valueOf(color.toString()));
-            x_old = x + deltaX;
-            y_old = y + deltaY;
-            //System.out.println("i: " + i + " ; x: " + x_old + " ; y: " + y_old);
+        vertexes_outX[0] = x;   // стартова позиція по горизонталі над центром зірки
+        vertexes_outY[0] = y;   // стартова позиція по вертикалі над центром зірки
+
+        for (int i = 1; i < vertexes_outX.length; i++){
+            deltaX = radius * Math.cos(Math.toRadians(SQUARE + i*CIRCLE/POLYGON));
+            deltaY = radius * (1 - Math.sin(Math.toRadians(SQUARE + i*CIRCLE/POLYGON)));
+            // координати наступної вершини, остання 6-та має співпадати з 1-ю
+            vertexes_outX[i] = x + deltaX;
+            vertexes_outY[i] = y + deltaY;
         }
-        return lines;
     }
 
-    private Line[] generatePentagon(double x, double y, double radius){
-        Color color = Color.color(1,
-                0,
-                0);
-        Line[] lines = new Line[POLYGON];
-        double deltaX;      // зміщення по горизонталі до наступної вершини :  R * cos(90° + 72°); 72° = 360° / 5
-        double deltaY;      // зміщення по вертикалі до наступної вершини :  R * (1 - sin(90° + 72°)); 72° = 360° / 5
-        double x_old = x + radius * Math.cos(Math.toRadians(SQUARE + 1/2 * (CIRCLE/POLYGON)));   // стартова позиція по горизонталі під центром зірки
-        double y_old = y + radius * (1 - Math.sin(Math.toRadians(SQUARE + 1/2 * (CIRCLE/POLYGON))));   // стартова позиція по вертикалі під центром зірки
-        //System.out.println("i = -1 ; x: " + x_old + " ; y: " + y_old);
-        for (int i = 0; i < lines.length; i++){
-            deltaX = radius * Math.cos(Math.toRadians(SQUARE + (2*(i+1)+1)/2 * (CIRCLE/POLYGON)));
-            deltaY = radius * (1 - Math.sin(Math.toRadians(SQUARE + (2*(i+1)+1)/2 * (CIRCLE/POLYGON))));
-            // сполучаєм дві послідовні по циклу вершини
-            lines[i] = new Line(x_old,
-                    y_old,
-                    x + deltaX,
-                    y + deltaY);
-            lines[i].setFill(Paint.valueOf(color.toString()));
-            x_old = x + deltaX;
-            y_old = y + deltaY;
-            //System.out.println("i: " + i + " ; x: " + x_old + " ; y: " + y_old);
+    // визначаєм координати внутрішніх вершин зірки
+    private void getVertexesIn(double x, double y, double radius){
+        double deltaX;      // зміщення по горизонталі від х до наступної вершини :  R * cos(90° + 36° + i*72°); 72° = 360°/5
+        double deltaY;      // зміщення по вертикалі від y до наступної вершини :  R * sin(90° + 36° + i*72°); 72° = 360°/5
+        vertexes_inX[0] = x + radius * Math.cos(Math.toRadians(SQUARE + CIRCLE/(2*POLYGON)));   // стартова позиція по горизонталі від центру зірки (зміщення: R * cos(90° + 36°))
+        vertexes_inY[0] = y - radius * Math.sin(Math.toRadians(SQUARE + CIRCLE/(2*POLYGON)));   // стартова позиція по вертикалі від центру зірки (зміщення: R * sin(90° + 36°))
+
+        for (int i = 1; i < vertexes_inX.length; i++){
+            deltaX = radius * Math.cos(Math.toRadians(SQUARE + CIRCLE/(2*POLYGON) + i*CIRCLE/POLYGON));
+            deltaY = radius * Math.sin(Math.toRadians(SQUARE + CIRCLE/(2*POLYGON) + i*CIRCLE/POLYGON));
+            // координати наступної вершини
+            vertexes_inX[i] = x + deltaX;
+            vertexes_inY[i] = y - deltaY;
         }
+    }
+
+    // заповнюєм кінцевий масив вершин об'єднуючи зовнішні вершини з внутрішніми
+    // зовнішні та внутрішні вершини додаємо по-черзі: раз - зовнішню, раз - внутрішню
+    private void getVertexes(){
+        int i_out = 0;
+        int i_in = 0;
+        for (int i = 0; i < vertexesX.length; i++){
+            if (i%2 == 0){      // для парних номерів записуєм зовнішні вершини
+                vertexesX[i] = vertexes_outX[i/2];
+                vertexesY[i] = vertexes_outY[i/2];
+            } else {        // для непарних номерів записуєм внутрішні вершини
+                vertexesX[i] = vertexes_inX[(i-1)/2];
+                vertexesY[i] = vertexes_inY[(i-1)/2];
+            }
+        }
+    }
+
+    // сполучаєм між собою вершини масиву vertexes
+    private Line[] getLines(double[] vX, double[] vY){
+        Line[] lines = new Line[vX.length];
+        for (int i = 0; i < lines.length-1; i++){
+            lines[i] = new Line(vX[i], vY[i], vX[i+1], vY[i+1]);
+        }
+        // останню вершину сполучаємо з першою
+        lines[lines.length-1] = new Line(vX[vX.length-1], vY[vX.length-1], vX[0], vY[0]);
         return lines;
     }
 
@@ -159,5 +211,37 @@ public class Main extends Application {
         System.out.print("Введіть радіус зірки: ");
         radius = sc.nextInt();
         sc.nextLine();
+        System.out.println("Оберіть метод виведення зображення зірки.");
+        System.out.println("1 - Метод1: 5 ліній із самоперетинами;     Будь-який інший символ - Метод2: 10 ліній без самоперетинів.");
+        System.out.print("Введіть символ для вибору методу: ");;
+        method = sc.nextLine();
     }
+
+    // метод для малювання внутрішнього п'ятикутника
+    // у фінальній версії не використовується
+    // служить лише для тестування
+    private Line[] generatePentagon(double x, double y, double radius){
+        Line[] lines = new Line[POLYGON];
+        double deltaX;      // зміщення по горизонталі від х до наступної вершини :  R * cos(90° + 36° + i*72°); 72° = 360°/5
+        double deltaY;      // зміщення по вертикалі від y до наступної вершини :  R * sin(90° + 36° + i*72°); 72° = 360°/5
+        double x_old = x + radius * Math.cos(Math.toRadians(SQUARE + CIRCLE/(2*POLYGON)));   // стартова позиція по горизонталі від центру зірки (зміщення: R * cos(90° + 36°))
+        double y_old = y - radius * Math.sin(Math.toRadians(SQUARE + CIRCLE/(2*POLYGON)));   // стартова позиція по вертикалі від центру зірки (зміщення: R * sin(90° + 36°))
+
+        System.out.println(x_old + "     " + y_old);
+
+        for (int i = 0; i < lines.length; i++){
+            deltaX = radius * Math.cos(Math.toRadians(SQUARE + CIRCLE/(2*POLYGON) + (i+1)*CIRCLE/POLYGON));
+            deltaY = radius * Math.sin(Math.toRadians(SQUARE + CIRCLE/(2*POLYGON) + (i+1)*CIRCLE/POLYGON));
+            // сполучаєм дві послідовні по циклу вершини
+            lines[i] = new Line(x_old,
+                    y_old,
+                    x + deltaX,
+                    y - deltaY);
+            x_old = x + deltaX;
+            y_old = y - deltaY;
+            System.out.println(x_old + "     " + y_old);
+        }
+        return lines;
+    }
+
 }
